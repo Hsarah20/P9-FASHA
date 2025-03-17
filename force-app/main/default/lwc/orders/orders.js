32d2735a9f02d1a5cf9d841066698c39f1583e43
Hasnaa Dessagne <hasnaliod@gmail.com>
2025-03-17 23:22:03 +0100

    feat-refacto && fix trigger handler + controller +lwc

--- force-app/main/default/lwc/orders/orders.js
+++ force-app/main/default/lwc/orders/orders.js
@@ -1,16 +1,35 @@
-import { LightningElement, api } from 'lwc';
-// TODO - récupérer la méthode apex permettant de faire ce calcul
+import { LightningElement, api,wire } from 'lwc';
+import getSumOrdersByAccount from '@salesforce/apex/AccountController.getSumOrdersByAccount';
 
 export default class Orders extends LightningElement {
 
-    sumOrdersOfCurrentAccount;
+    sumOrdersOfCurrentAccount=0;
     @api recordId;
 
     connectedCallback() {
         this.fetchSumOrders();
     }
 
-    fetchSumOrders() {
-        // TODO - récupérer le montant total des Orders sur le compte avec la méthode apex
+    // fetchSumOrders() {
+    //     // TODO - récupérer le montant total des Orders sur le compte avec la méthode apex
+    // }
+
+    @wire(getSumOrdersByAccount, { accountId: '$recordId' })
+    wiredOrder({ error, data }) {
+        if (data) {
+            this.sumOrdersOfCurrentAccount = data;
+                
+        } else if (error) {
+            console.error('Erreur lors de la récupération de la somme des commandes', error);
+        }
     }
+
+    get isError() {
+        return !this.sumOrdersOfCurrentAccount || this.sumOrdersOfCurrentAccount <= 0;
+    }
+
+    get isSuccess() {
+        return this.sumOrdersOfCurrentAccount > 0;
+    }
+    
 }
